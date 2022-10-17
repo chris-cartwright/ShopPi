8703cae0ba52ddf95b8327a464456fcbe70527a8
Chris Cartwright <cartwright.christopher@gmail.com>
2022-10-17 17:41:49 -0500

    Fix permission issues

--- Server/Scripts/screen_off.sh
+++ Server/Scripts/screen_off.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-sudo bash -c "echo 1 > /sys/class/backlight/rpi_backlight/bl_power"
+echo 1 > /sys/class/backlight/rpi_backlight/bl_power
--- Server/Scripts/screen_on.sh
+++ Server/Scripts/screen_on.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-sudo bash -c "echo 0 > /sys/class/backlight/rpi_backlight/bl_power"
+echo 0 > /sys/class/backlight/rpi_backlight/bl_power
